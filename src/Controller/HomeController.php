3901a324e70a071188ebbc273f8d2dbc1ef21928
Crisme24 <crisme-24@hotmail.com>
2020-11-04 09:49:03 +0000

    se agrego subasta.sql con los datos de la base de datos

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Puja;
 use App\Entity\Subasta;
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,4 +25,5 @@ class HomeController extends AbstractController
             'pujas' => $pujas[0]
         ]);
     }
+    
 }
